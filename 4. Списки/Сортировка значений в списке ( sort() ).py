ced89a2772cbdd52c28458a91f08fb644c8a0c11
RomanenkoAnton <toshik06rom@rambler.ru>
2021-02-20 12:54:44 +0300

    1

--- 4. Списки/Сортировка значений в списке ( sort() ).py
+++ 4. Списки/Сортировка значений в списке ( sort() ).py
@@ -1,1 +1,18 @@
-spam = 
+spam = [2, 5, 3.14, 1, -7]
+spam.sort()
+print(spam)
+
+spam2 = ['ants', 'cats', 'dogs', 'badgers', 'elephants']
+spam2.sort()
+print(spam2)
+
+spam.sort(reverse=True)
+print(spam)
+
+spam3 = ['Alice', 'ants', 'Bob', 'badgers', 'Carol', 'cats']
+spam3.sort()
+print(spam3)
+
+spam4 = ['a', 'z', 'A', 'Z']
+spam4.sort(key=str.lower)
+print(spam4)
