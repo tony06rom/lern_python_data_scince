2e125220cc7189a311949b28e752a9ded1e3fd0f
tony06rom <65313046+tony06rom@users.noreply.github.com>
2021-02-20 09:56:24 +0300

    Update Последовательность Коллатца.py

--- Последовательность Коллатца.py
+++ Последовательность Коллатца.py
@@ -1,18 +1,19 @@
-def collatz(number):
-    if number % 2 == 0:
-        return number // 2
-    elif number % 2 == 1:
-        return 3 * number + 1
+def collatz(number):  # функция от значения
+    if number % 2 == 0:   # если зачение является четным числом
+        return number // 2   # напечатать значение деленное на 2
+    elif number % 2 == 1:   # если зачение не является четным числом
+        return 3 * number + 1   # напечатать значение умноженное на 3 и прибавить 1
 
-print('Введите целове число: ')
+print('Введите целове число: ')   # выводит на экран 
 
 try:
-    intInput = int(input())
+    intInput = int(input())   # ввод числа
 except:
-    print('Введено не целове число, повторите ввод...')
+    print('Введено не целове число, повторите ввод...')   # происходит отладка ошибки и выводит сообщение
+    return intInput   #  возвращает повторный ввод
 
-while intInput != 1:
-    intInput = collatz(intInput)
-    print(intInput)
+while intInput != 1:   # пока введенное число не будет равно 1
+    intInput = collatz(intInput)   # введенное число будет подставляться в функцию collatz вместо number
+    print(intInput)   # выводит фунцию collatz() с уже введенным значением в самом начале ( intInput = int(input()) )
 
 
