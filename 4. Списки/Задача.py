db53d03a9f37c94a5b1ab8e0244bb3b489fabc27
RomanenkoAnton <toshik06rom@rambler.ru>
2021-02-26 09:59:16 +0300

    1

--- 4. Списки/Задача.py
+++ 4. Списки/Задача.py
@@ -1,7 +1,8 @@
 spam = ['apples', 'bananes', 'tofu', 'cats']
 
 def myDef(spam):
+    spam[-1] = 'and ' + spam[-1]
     for i in spam:
-        return
+            print(i + ', ', end = '')
 
 myDef(spam)
