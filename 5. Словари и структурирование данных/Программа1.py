bac7ac0487ea0bd265c943a62a7612639bd0d3fc
RomanenkoAnton <toshik06rom@rambler.ru>
2021-02-26 15:15:39 +0300

    1

--- 5. Словари и структурирование данных/Программа1.py
+++ 5. Словари и структурирование данных/Программа1.py
@@ -2,17 +2,17 @@
 
 birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Color': 'Mar 4'}
 
-while True:
+while True: # Пока правда - выполнять код
     print('Enter a name: (blank to quit)')
-    name = input()
-    if name == '':
-        break
-
-    if name in birthdays:
-        print(birthdays[name] + ' is the birthday of ' + name)
-    else:
-        print('I do not have birthday information of ' + name)
+    name = input() # Ввод нового имени
+    if name == '': # Если имя будет равно пустоте (два раза Enter)
+        print('End program')
+        break # выйти из цикла
+    if name in birthdays: # Если имя уже содержится в словаре
+        print(birthdays[name] + ' is the birthday of ' + name) # Напечатать значение имени в списке + текс + имя
+    else: # Если ничего из перечисленного не выполняется
+        print('I do not have birthday information of ' + name) # Напечатать текст + имя
         print('What is their birthday?')
-        bday = input()
-        birthdays[name] = bday
+        bday = input() # Ввод возраста для нового имени
+        birthdays[name] = bday # Возраст вносится в словать с ключем введенного нового имени
         print('Birthday database update.')
