b4efddc4d846329a208db4c897c0f40fc0413fd7
RomanenkoAnton <toshik06rom@rambler.ru>
2021-03-01 16:53:27 +0300

    1

---
+++ 5. Словари и структурирование данных/Красивая печать.py
@@ -0,0 +1,12 @@
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
+# или
+#print(pprint.pformat(count))
