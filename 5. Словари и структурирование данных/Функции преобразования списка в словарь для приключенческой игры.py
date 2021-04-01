f0119afe261e9d3caada7ba539c509cfc659b763
RomanenkoAnton <toshik06rom@rambler.ru>
2021-04-01 08:16:41 +0300

    1

--- 5. Словари и структурирование данных/Функции преобразования списка в словарь для приключенческой игры.py
+++ 5. Словари и структурирование данных/Функции преобразования списка в словарь для приключенческой игры.py
@@ -2,10 +2,18 @@ Inventory = {'gold': 999, 'gun': 3, 'water': 5, 'meat': 7, 'health': 2, 'stuff':
 
 dragonLoot = ['gold', 'gun', 'water', 'gold', 'stuff', 'gun', 'health', 'gold']
 
-def addToInventory(inventory, addItems):
-    print('Loot:')
-    dropDragon = 0
-    for i in dragonLoot:
+def addToInventory(inventory, addedItems):
+
+
+def displayInventory(inventory):
+    print('Inventory:')
+    item_total = 0
+    for k, v in inventory.items():
+        print(k + ' = ' + str(v))
+        item_total += v
+    print('Total number of items: ' + str(item_total))
+
+displayInventory(Inventory)
 
 
 """def displayInventory(inventory):
